7308286e6a13d3ecf376c0e508e7be73df1f74a6
rohanbagadi2005-coder <rohanbagadi2005@gmail.com>
2026-08-30 23:07:29 +0530

    Time: 6 ms (9.28%), Space: 8.9 MB (22.33%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.c
@@ -0,0 +1,17 @@
+bool isPalindrome(int x) {
+    if(x<0)
+    return false;
+
+    int original = x;
+    long long reverse = 0;
+
+    while (x > 0)
+    {
+        reverse = reverse * 10 + x % 10;
+        x = x / 10;
+    }
+    if(original == reverse)
+        return true;
+    else
+        return false;
+}
